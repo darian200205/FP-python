66112e31475640c1d77fa14cca3656f23c7e84dd
Darian <darianofficialchannelvlog@gmail.com>
2021-08-03 16:34:38 +0300

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def isPrime(num):
+    if n == 1:
+        return 0
+    for i in range(2, math.sqrt(num) + 1):
+        if num % i == 0:
+            return 0
+    return 1
+
+def firstPrime(num):
+    ans = num + 1
+    while isPrime(ans) == 0:
+        ans = ans + 1
+    return ans
+
+def age(Day, Month, Year):
+    curr = datetime.datetime.now()
+    ans = 0
+    ans += (curr.year - Year - 1) * 365
+    ans += (curr.month * 30) + curr.day
+    ans += (30 - Day) + (12 - Month) * 30
+    return ans
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    task = int(input())
+    if task == 1:
+        number = int(input())
+        print(firstPrime(num))
+    elif task == 2:
+        Day = int(input())
+        Month = int(input())
+        Year = int(input())
+        print(age(Day, Month, Year))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
