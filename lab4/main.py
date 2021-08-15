66b1a860b4b2e6fde77d7eadc0c4160e990b9d00
Darian <darianofficialchannelvlog@gmail.com>
2021-08-15 19:33:35 +0300

    added new dict

--- lab4/main.py
+++ lab4/main.py
@@ -1,34 +1,22 @@
 from user_menu import right_operation
 from do_task import do_task_1, do_task_2, do_task_3, do_task_4, do_task_5
 
-
 if __name__ == '__main__':
-    menu = {'1': "Adaugă număr în listă. ", '2': "Modifică elemente din listă. ", '3': "Căutare numere. ",
-            '4': "Operații cu numerele din listă ", '5': "Filtrare. ", '6': "Iesire "}
 
     list = []
     aux = []
-    while True:
 
-        for options in menu:
+    show_menu = {'1': "Adauga numar in lista", '2': "Modifică elemente din listă. ", '3': "Căutare numere. ",
+                 '4': "Operații cu numerele din listă ", '5': "Filtrare. ", '6': "Iesire "}
+
+    functionality_menu = {1: do_task_1, 2: do_task_2, 3: do_task_3,
+                          4: do_task_4, 5: do_task_5, 6: quit}
+
+    while True:
+        for options in show_menu:
             print(options, end=" ")
-            print(menu[options])
+            print(show_menu[options])
 
         _selected = right_operation(1, 6, "Introdu un numar:")
-        if _selected == 1:
-            do_task_1(list)
-
-        elif _selected == 2:
-            do_task_2(list)
-
-        elif _selected == 3:
-            do_task_3(list)
-
-        elif _selected == 4:
-            do_task_4(list)
-
-        elif _selected == 5:
-            do_task_5(list)
-
-        elif _selected == 6:
-            quit(list)
+        task = functionality_menu[_selected]
+        task(list)
