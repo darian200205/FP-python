14d5e22fc59ad9b1dbf9710d7c7f1d4fac4d753f
darian200205 <darianserban@yahoo.com>
2021-09-14 13:02:21 +0300

    added classes
    added functionality to grade a specific student (given the student id) at a given subject (given the subject id).
    the grades list for each student looks like this:
    grades = ['subject_id', grade1, grade2, ..., gradeN]
    and we check every time if the student already has 1 grade for the given subject id, if so we just add the grade to the list, if not we create the list for the given subject_id and in the end it will look like this:
    grade = [
    [subject_id1, grade1, grade2],
    [subject_id2, grade1, grade2, grade3],
    ...,
    [subject_idN, grade1]
    ].

--- lab7/main.py
+++ lab7/main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+show_menu = ["1: Adauga"
+             "2: Sterge",
+             "3:Cautare",
+             "4:Asignare de note",
+             "5:Statistici"
+             ]
 
 
-# Press the green button in the gutter to run the script.
+class Student:
+    def __init__(self, student_id, student_name):
+        self.student_id = student_id
+        self.student_name = student_name
+        self.grades = []
+
+    def show_students(self):
+        print(str(self.student_name) + ':' + str(self.grades))
+
+
+class Subject:
+    def __init__(self, subject_id, subject_name, teacher):
+        self.subject_id = subject_id
+        self.subject_name = subject_name
+        self.teacher = teacher
+
+
+student_list = []
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    id_student = int(input("id student:"))
+    name_student = input("name:")
+    student_list.append(Student(id_student, name_student))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    subject_id = int(input("id-ul disciplinei:"))
+    grade = input("introduceti nota:")
+    student_to_grade = int(input("introduceti id-ul studentului:"))
+
+    for i in range(0, len(student_list)):
+        if student_list[i].student_id == student_to_grade:
+            student_list[i].grades.append([subject_id, grade])
+            break
+
+    subject_id = int(input("id-ul disciplinei:"))
+    grade = input("introduceti nota:")
+    student_to_grade = int(input("introduceti id-ul studentului:"))
+
+    for i in range(0, len(student_list)):
+        if student_list[i].student_id == student_to_grade:
+            exists = False
+            for j in range(0, len(student_list[i].grades)):
+                if student_list[i].grades[j][0] == subject_id:
+                    exists = True
+                    student_list[i].grades[j].append(grade)
+                    break
+            if not exists:
+                student_list[i].grades.append([subject_id, grade])
+
+    for i in student_list:
+        i.show_students()
