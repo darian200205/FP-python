fb4a3ca726f22e67cf078de8cee87331fc3888b6
Darian <darianserban@yahoo.com>
2021-09-21 20:42:32 +0300

    made lists private

--- lab7/Controller/AppController.py
+++ lab7/Controller/AppController.py
@@ -31,8 +31,8 @@ class AppController:
                 self.ui_representer.show_error()
                 continue
 
-            self.ui_representer.show_student_list(self.memory_representer.student_list)
-            self.ui_representer.show_subject_list(self.memory_representer.subjects_list)
+            self.ui_representer.show_student_list(self.memory_representer.get_student_list())
+            self.ui_representer.show_subject_list(self.memory_representer.get_subject_list())
 
     def task1(self):
         self.ui_representer.show_add_menu()
@@ -65,32 +65,30 @@ class AppController:
         if task == 1:
             searched_student = self.ui_representer.get_student_name()
 
-            if self.search_student(searched_student, self.memory_representer.student_list) != -1:
-                self.search_student(searched_student,
-                                                       self.memory_representer.student_list).show_students()
+            if self.memory_representer.search_student(searched_student) != -1:
+                self.memory_representer.search_student(searched_student).show_students()
             else:
                 self.ui_representer.show_error()
 
         elif task == 2:
             searched_subject = self.ui_representer.get_subject_name()
 
-            if self.search_subject(searched_subject, self.memory_representer.subjects_list) != -1:
-                self.search_subject(searched_subject,
-                                                       self.memory_representer.subjects_list).show_subjects()
+            if self.memory_representer.search_subject(searched_subject) != -1:
+                self.memory_representer.search_subject(searched_subject).show_subjects()
             else:
                 self.ui_representer.show_error()
 
     def task4(self):
-        if not self.memory_representer.subjects_list:
+        if not self.memory_representer.get_subject_list():
             self.ui_representer.show_error()
         else:
             student_to_grade = self.ui_representer.get_student_id()
-            while not self.student_exists(student_to_grade, self.memory_representer.student_list):
+            while not self.student_exists(student_to_grade, self.memory_representer.get_student_list()):
                 self.ui_representer.show_error()
                 student_to_grade = self.ui_representer.get_student_id()
 
             id_of_subject = self.ui_representer.get_subject_id()
-            while not self.subject_exists(id_of_subject, self.memory_representer.subjects_list):
+            while not self.subject_exists(id_of_subject, self.memory_representer.get_subject_list()):
                 self.ui_representer.show_error()
                 id_of_subject = self.ui_representer.get_subject_id()
 
@@ -105,17 +103,17 @@ class AppController:
 
         task = self.ui_representer.get_task()
         if task == 1:
-            sorted_students_by_name = self.sort_students_by_name()
+            sorted_students_by_name = self.memory_representer.sort_students_by_name()
             self.ui_representer.show_student_list(sorted_students_by_name)
 
         elif task == 2:
             target_subject = self.ui_representer.get_subject_id()
-            sorted_students_by_average = self.sort_by_subject_grades(target_subject)
+            sorted_students_by_average = self.memory_representer.sort_by_subject_grades(target_subject)
             self.ui_representer.show_student_list(sorted_students_by_average)
 
         elif task == 3:
             best_students = self.sort_by_average()[0]
-            number_of_students = self.sort_by_average()[1]
+            number_of_students = self.memory_representer.sort_by_average()[1]
             self.ui_representer.show_best_students(best_students, number_of_students)
 
         else:
@@ -138,45 +136,6 @@ class AppController:
         else:
             self.ui_representer.show_error()
 
-    def search_student(self, searched_student, students):
-        for student in students:
-            if student.student_name == searched_student:
-                return student
-        return -1
 
-    def search_subject(self, searched_subject, subjects):
-        for subject in subjects:
-            if subject.subject_name == searched_subject:
-                return subject
-        return -1
 
-    def student_exists(self, current_id, students):
-        for student in students:
-            if student.student_id == current_id:
-                return True
-        return False
 
-    def subject_exists(self, current_subject, subjects):
-        for subject in subjects:
-            if subject.subject_id == current_subject:
-                return True
-        return False
-
-    def sort_students_by_name(self):
-        sorted_students_by_name = sorted(self.memory_representer.student_list, key=lambda x: x.student_name, reverse=False)
-        return sorted_students_by_name
-
-    def sort_by_subject_grades(self, target_subject):
-        for student in range(0, len(self.memory_representer.student_list[0].grades)):
-            if self.memory_representer.student_list[0].grades[student][0] == target_subject:
-                target = student
-
-        sorted_students_by_average = sorted(self.memory_representer.student_list, key=lambda x: x.grades[target][2], reverse=True)
-        return sorted_students_by_average
-
-    def sort_by_average(self):
-        for student in range(0, len(self.memory_representer.student_list)):
-            self.memory_representer.student_list[student].average = self.memory_representer.student_list[student].calculate_average_grades()
-        best_students = sorted(self.memory_representer.student_list, key=lambda x: x.average, reverse=True)
-        number_of_students = 20 * len(self.memory_representer.student_list) / 100
-        return [best_students, number_of_students]
--- lab7/Repository/InMemoryRepository.py
+++ lab7/Repository/InMemoryRepository.py
@@ -4,56 +4,62 @@ from UI.UI_Representer import UIRepresenter
 
 
 class InMemoryRepository:
-    student_list = []
-    subjects_list = []
+    __student_list = []
+    __subjects_list = []
 
     def __init__(self):
         self.ui_representer = UIRepresenter()
 
+    def get_student_list(self):
+        return self.__student_list
+
+    def get_subject_list(self):
+        return self.__subjects_list
+
     def add_student(self, id_of_student, name_of_student):
-        self.student_list.append(Student(id_of_student, name_of_student))
-        for i in range(0, len(self.subjects_list)):
-            self.student_list[len(self.student_list) - 1].grades.append(
-                [self.subjects_list[i].subject_id, "Media", 0, "Notele"])
+        self.__student_list.append(Student(id_of_student, name_of_student))
+        for i in range(0, len(self.__subjects_list)):
+            self.__student_list[len(self.__student_list) - 1].grades.append(
+                [self.__subjects_list[i].subject_id, "Media", 0, "Notele"])
 
     def add_subject(self, id_of_subject, name_of_subject, name_of_teacher):
-        self.subjects_list.append(Subject(id_of_subject, name_of_subject, name_of_teacher))
-        for i in range(0, len(self.student_list)):
-            self.student_list[i].grades.append([id_of_subject, "Media", 0, "Notele"])
+        self.__subjects_list.append(Subject(id_of_subject, name_of_subject, name_of_teacher))
+        for i in range(0, len(self.__student_list)):
+            self.__student_list[i].grades.append([id_of_subject, "Media", 0, "Notele"])
 
     def delete_student(self, student_name):
-        for i in self.student_list:
+        for i in self.__student_list:
             if i.student_name == student_name:
-                self.student_list.remove(i)
+                self.__student_list.remove(i)
 
     def delete_subject(self, subject_to_delete):
-        for i in self.subjects_list:
+        for i in self.__subjects_list:
             if i.subject_name == subject_to_delete:
-                self.subjects_list.remove(i)
+                self.__subjects_list.remove(i)
 
     def grade_student(self, grade_, id_of_student, id_of_subject):
-        for student in range(0, len(self.student_list)):
-            if self.student_list[student].student_id == id_of_student:
-                for grade in range(0, len(self.student_list[student].grades)):
-                    if self.student_list[student].grades[grade][0] == id_of_subject:
-                        self.student_list[student].grades[grade].append(grade_)
-                        self.student_list[student].grades[grade][2] += grade_
-                        self.student_list[student].grades[grade][2] = self.student_list[student].grades[grade][2] / (
-                                len(self.student_list[student].grades[grade]) - 4)
+        for student in range(0, len(self.__student_list)):
+            if self.__student_list[student].student_id == id_of_student:
+                for grade in range(0, len(self.__student_list[student].grades)):
+                    if self.__student_list[student].grades[grade][0] == id_of_subject:
+                        self.__student_list[student].grades[grade].append(grade_)
+                        self.__student_list[student].grades[grade][2] += grade_
+                        self.__student_list[student].grades[grade][2] = self.__student_list[student].grades[grade][2] / (
+                                len(self.__student_list[student].grades[grade]) - 4)
                         break
 
     def modify_student(self, modify_what, target_student):
         if modify_what == "NUME":
             new_name = self.ui_representer.get_student_name()
-            for student in range(0, len(self.student_list)):
-                if self.student_list[student].student_name == target_student:
-                    self.student_list[student].student_name = new_name
+            for student in range(0, len(self.__student_list)):
+                if self.__student_list[student].student_name == target_student:
+                    self.__student_list[student].student_name = new_name
 
         elif modify_what == "ID":
             new_id = self.ui_representer.get_student_id()
-            for student in range(0, len(self.student_list)):
-                if self.student_list[student].student_name == target_student:
-                    self.student_list[student].student_id = new_id
+            for student in range(0, len(self.__student_list)):
+                if self.__student_list[student].student_name == target_student:
+                    self.__student_list[student].student_id = new_id
 
         else:
             self.ui_representer.show_error()
@@ -61,21 +67,64 @@ class InMemoryRepository:
     def modify_subject(self, modify_what, target_subject):
         if modify_what == "NUME":
             new_name = self.ui_representer.get_subject_name()
-            for i in range(0, len(self.subjects_list)):
-                if self.subjects_list[i].subject_id == target_subject:
-                    self.subjects_list[i].subject_name = new_name
+            for i in range(0, len(self.__subjects_list)):
+                if self.__subjects_list[i].subject_id == target_subject:
+                    self.__subjects_list[i].subject_name = new_name
 
         elif modify_what == "ID":
             new_id = self.ui_representer.get_subject_id()
-            for i in range(0, len(self.subjects_list)):
-                if self.subjects_list[i].subject_id == target_subject:
-                    self.subjects_list[i].subject_id = new_id
+            for i in range(0, len(self.__subjects_list)):
+                if self.__subjects_list[i].subject_id == target_subject:
+                    self.__subjects_list[i].subject_id = new_id
 
         elif modify_what == "PROFESOR":
             new_teacher = self.ui_representer.get_teacher_name()
-            for i in range(0, len(self.subjects_list)):
-                if self.subjects_list[i].subject_id == target_subject:
-                    self.subjects_list[i].teacher = new_teacher
+            for i in range(0, len(self.__subjects_list)):
+                if self.__subjects_list[i].subject_id == target_subject:
+                    self.__subjects_list[i].teacher = new_teacher
 
         else:
             self.ui_representer.show_error()
+
+    def search_student(self, searched_student):
+        for student in self.__student_list:
+            if student.student_name == searched_student:
+                return student
+        return -1
+
+    def search_subject(self, searched_subject):
+        for subject in self.__subjects_list:
+            if subject.subject_name == searched_subject:
+                return subject
+        return -1
+
+    def student_exists(self, current_id, __student_list):
+        for student in self.__student_list:
+            if student.student_id == current_id:
+                return True
+        return False
+
+    def subject_exists(self, current_subject):
+        for subject in self.__subjects_list:
+            if subject.subject_id == current_subject:
+                return True
+        return False
+
+    def sort_students_by_name(self):
+        sorted_students_by_name = sorted(self.__student_list, key=lambda x: x.student_name, reverse=False)
+        return sorted_students_by_name
+
+    def sort_by_subject_grades(self, target_subject):
+        for student in range(0, len(self.__student_list[0].grades)):
+            if self.__student_list[0].grades[student][0] == target_subject:
+                target = student
+
+        sorted_students_by_average = sorted(self.__student_list, key=lambda x: x.grades[target][2], reverse=True)
+        return sorted_students_by_average
+
+    def sort_by_average(self):
+        for student in range(0, len(self.__student_list)):
+            self.__student_list[student].average = self.__student_list[student].calculate_average_grades()
+        best_students = sorted(self.__student_list, key=lambda x: x.average, reverse=True)
+        number_of_students = 20 * len(self.__student_list) / 100
+        return [best_students, number_of_students]
